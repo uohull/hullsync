906c0c3ba43cd2b1604c5a4f26cc3be112627bd2
Martyn Whitwell <martyn.whitwell@gmail.com>
2016-12-29 20:08:05 +0000

    Use local redis, don't store keys in git

--- app/jobs/watch_processor_job.rb
+++ app/jobs/watch_processor_job.rb
@@ -4,10 +4,18 @@ class WatchProcessorJob
 
   def self.perform(message)
     if message['event_type'] == 'added_collaborator'
-      puts '1. ----------------------------------'
-      puts 'In watch processor'
+
+      logger.info("1. ---------------------------")
+      logger.info('In watch processor')
+      logger.info("Event:\t#{params[:event_type]}")
+      logger.info("Item Name:\t#{params[:item_name]}")
+      logger.info("Item Type:\t#{params[:item_type]}")
+      logger.info("Item Id:\t#{params[:item_id]}")
+
+
+
       # if event is 'added collaborator' act on it
-      box_client = BoxClient.new
+      box_client = BoxClient.new()
       # Get folder metadata
       folder_id = message['item_id']
       folder = box_client.folder_from_id(folder_id)
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-Resque.redis = "redis://188.166.155.21:6379"
+Resque.redis = "redis://localhost:6379"
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
