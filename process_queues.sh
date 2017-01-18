249bbb9106b2f9f2e5077b0ed28bd93283ef60c2
Martyn Whitwell <martyn.whitwell@gmail.com>
2017-01-18 14:43:15 +0000

    Change logging

--- process_queues.sh
+++ process_queues.sh
@@ -9,8 +9,8 @@ fi
 cd /var/hullsync/
 
 # clear old queues
-nohup bundle exec rake RAILS_ENV=production resque:clear
+bundle exec rake RAILS_ENV=production resque:clear >> /var/hullsync/log/resque.log
 
 # start workers in background
-nohup bundle exec rake QUEUE=* RAILS_ENV=production environment resque:work &
+nohup bundle exec rake QUEUE=* RAILS_ENV=production environment resque:work >> /var/hullsync/log/resque.log &
 
